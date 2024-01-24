9e66911b2af13ffb1199e191b7576ac86d4fc7bd
rubiaqute <rubiaqute@gmail.com>
2024-01-24 23:08:12 +0300

    feature: list implementing

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import path from "path";
+import fs from "fs";
+import { ERROR_MESSAGE, _dirname } from "./utils.js";
+
 const list = async () => {
-    // Write your code here 
+  const pathToFolder = path.resolve(_dirname, 'files');
+  fs.access(pathToFolder, (error)=>{
+    if (error) {
+        throw Error(ERROR_MESSAGE);
+    } else {
+        fs.readdir(pathToFolder, { withFileTypes: true }, (error, files) => {
+          const fileNames = files.map((file) => file.name);
+          console.log(fileNames);
+        });
+    }
+  });
 };
 
 await list();
