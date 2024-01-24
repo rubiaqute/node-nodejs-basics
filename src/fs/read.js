b6e21178c6bee484b756544c16c10f0c5f89eab3
rubiaqute <rubiaqute@gmail.com>
2024-01-24 23:15:49 +0300

    feature: read implementing

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import path from "path";
+import fs from "fs";
+import { ERROR_MESSAGE, _dirname } from "./utils.js";
+
 const read = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve(_dirname, "files", 'fileToRead.txt');
+
+  fs.access(pathToFile, async (error) => {
+    if (error) {
+        throw Error(ERROR_MESSAGE);
+    } else {
+        const fileContent = await fs.promises.readFile(pathToFile, {
+          encoding: "utf8",
+        });
+        console.log(fileContent);
+    }
+  });
 };
 
 await read();
