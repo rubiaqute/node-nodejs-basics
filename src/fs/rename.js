46e76a557bee494a067dfcab0178648042bae3e8
rubiaqute <rubiaqute@gmail.com>
2024-01-24 22:50:14 +0300

    feature: rename implementing

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import path from "path";
+import url from "url";
+import fs from "fs";
+import { ERROR_MESSAGE } from "./utils.js";
+
+
 const rename = async () => {
-    // Write your code here 
+    const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const pathToOriginalFile = path.resolve(
+      _dirname,
+      "files",
+      "wrongFilename.txt"
+    );
+
+    const pathToNewFile = path.resolve(_dirname, "files", "properFilename.md");
+
+    fs.access(pathToOriginalFile, (error) => {
+      if (error) {
+        throw Error(ERROR_MESSAGE);
+      } else {
+
+        fs.access(pathToNewFile, (error) => {
+          if (error) {
+            
+            fs.promises.rename(pathToOriginalFile, pathToNewFile);
+          } else {
+           throw Error(ERROR_MESSAGE);
+          }
+        });
+      }
+    });
 };
 
 await rename();
