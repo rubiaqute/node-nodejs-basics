5a290ac4bfce81e8b8d428da18e00623991ef9f0
rubiaqute <rubiaqute@gmail.com>
2024-01-27 11:08:15 +0300

    feature: zip functions implemented

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import path from "path";
+import fs from "fs";
+import { pipeline } from "stream/promises";
+import zlib from "zlib";
+import url from "url";
+
 const decompress = async () => {
-    // Write your code here 
+      const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+      const pathToOriginalFile = path.resolve(
+        _dirname,
+        "files",
+        "fileToCompress.txt"
+      );
+      const pathToArchiveFile = path.resolve(_dirname, "files", "archive.gz");
+
+      fs.access(pathToArchiveFile, async (error) => {
+        if (error) {
+          throw Error("No such file");
+        } else {
+          await pipeline(
+            fs.createReadStream(pathToArchiveFile),
+            zlib.createUnzip(),
+            fs.createWriteStream(pathToOriginalFile)
+          );
+        }
+      });
 };
 
 await decompress();
