96b35c4b2256a5a9d8ee71909a7c0a4e86fa13ca
rubiaqute <rubiaqute@gmail.com>
2024-01-27 11:57:19 +0300

    feature: cp implementing

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import * as childProcess from "child_process";
+import path from "path";
+import * as url from "url";
+import { stdout, stdin } from "process";
+
+export const spawnChildProcess = async (args) => {
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "script.js");
+
+  const child = childProcess.spawn("node", [filePath, ...args], {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  stdin.pipe(child.stdin);
+  child.stdout.pipe(stdout);
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([45, 67]);
